7c633600a16f9d1c416cad383efa3f0f2e8c6053
Mehwish4610 <125643042+Mehwish4610@users.noreply.github.com>
2024-01-17 12:39:34 +0530

    Update Program10.cs

--- Program10.cs
+++ Program10.cs
@@ -1,59 +1,70 @@
 using System;
-public class Shape
+
+public abstract class Shape
 {
-public virtual void draw()
-{
-Console.WriteLine("Drawing Shape");
+    public virtual void Draw()
+    {
+        Console.WriteLine("Drawing Shape");
+    }
+
+    public virtual void Erase()
+    {
+        Console.WriteLine("Erasing Shape");
+    }
 }
-public virtual void erase()
-{
-Console.WriteLine("Erasing Shape");
-}
-}
+
 public class Circle : Shape
 {
-public override void draw()
-{
-Console.WriteLine("Drawing Circle");
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing Circle");
+    }
+
+    public override void Erase()
+    {
+        Console.WriteLine("Erasing Circle");
+    }
 }
-public override void erase()
-{
-Console.WriteLine("Erasing Circle");
-}
-}
+
 public class Triangle : Shape
 {
-public override void draw()
-{
-Console.WriteLine("Drawing Triangle");
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing Triangle");
+    }
+
+    public override void Erase()
+    {
+        Console.WriteLine("Erasing Triangle");
+    }
 }
-public override void erase()
-{
-Console.WriteLine("Erasing Triangle");
-}
-}
+
 public class Square : Shape
 {
-public override void draw()
-{
-Console.WriteLine("Drawing Square");
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing Square");
+    }
+
+    public override void Erase()
+    {
+        Console.WriteLine("Erasing Square");
+    }
 }
-public override void erase()
-{
-Console.WriteLine("Erasing Square");
-}
-}
+
 public class Program10
 {
-public static void Main()
-{
-Shape[] shapes = new Shape[3];
-shapes[0] = new Circle();
-shapes[1] = new Triangle();
-shapes[2] = new Square();
-foreach (Shape shape in shapes)
-{
-shape.draw(); shape.erase();
-}
-}
+    public static void Main()
+    {
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle();
+        shapes[1] = new Triangle();
+        shapes[2] = new Square();
+
+        foreach (Shape shape in shapes)
+        {
+            shape.Draw();
+            shape.Erase();
+        }
+    }
 }
